44be1ab59ccd61c9e75d3c54a68af66e47df2dbb
Rodrigo Gómez Maitret <green.tie.rgm@gmail.com>
2024-01-04 18:09:58 -0600

    Optimize for Windows, enable CLI usage.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 import flake8.main.application as f8
-from dotenv import load_dotenv
 from rich import print
 from rich.table import Table
 
@@ -24,42 +23,43 @@ class FakeFile(io.StringIO):
     def getvalue(self):
         return super().getvalue()
 
+def main():
+    dir = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()
+    report = flake8_to_dict(dir)
+    markdown_file_path = 'flake8_report.md'
+    print_flake8_report(report, markdown_file_path)
 
 def flake8_to_dict(path_to_code):
     base_dir = os.path.commonpath([path_to_code])
-    
     # Temporary redirect stdout to the custom FakeFile
     original_stdout = sys.stdout
     sys.stdout = FakeFile()
-
     # Initialize and run flake8
     app = f8.Application()
     app.initialize([
-        '--exclude=venv,.venv,your_project/external_packages,*/site-packages/*',
+        '--exclude=venv,.venv,wenv,.wenv,your_project/external_packages,*/site-packages/*',
         path_to_code
     ])
     app.run_checks()
     app.report()
-
     # Capture and restore stdout
     flake8_output = sys.stdout.getvalue()
     sys.stdout = original_stdout
-
     # Parse the flake8 output
     results = {}
     for line in flake8_output.strip().split('\n'):
         if line:  # Ensure the line is not empty
-            file, line_no, col_no, msg = line.split(':', 3)
+            file, line_no, col_no, msg = line.rsplit(':', 3)
             # Remove base directory from file path
             relative_path = os.path.relpath(file, base_dir)
             error_code = msg.strip().split(' ')[0]
+            # Append the issue to the results dict
             results.setdefault(relative_path, []).append({
                 'line': int(line_no),
                 'column': int(col_no),
                 'error_code': error_code,
                 'message': msg.strip()[5:]
             })
-
     return results
 
 def md_link(content, link):
@@ -67,24 +67,21 @@ def md_link(content, link):
 
 
 def print_flake8_report(report, markdown_file_path):
-    with open(markdown_file_path, 'w') as md_file:
-        md_file.write("# Judge Report\n")
+    with open(markdown_file_path, 'w', encoding="utf-8") as md_file:
+        md_file.write("# ⚖️ Judge Report\n")
         md_file.write("## ❄️ flake8\n")
-        md_file.write(f"Review for {md_link('PEP8', LINK_PEP8)} compliance.\n")
+        md_file.write(f"Python code review for {md_link('PEP8', LINK_PEP8)} compliance.\n")
         for file_path, issues in report.items():
-            table = Table(title=file_path)
-
+            table = Table(title=file_path, title_justify="left")
             # Define columns
             table.add_column("Line", justify="right")
             table.add_column("Col.", justify="right")
             table.add_column("Code")
             table.add_column("Message")
-
             # Markdown table header
-            md_file.write(f"### {file_path}\n\n")
+            md_file.write(f"### 📄 `{file_path}`\n\n")
             md_file.write("| Line | Col. | Code | Message |\n")
             md_file.write("|-----:|-----:|------|---------|\n")
-
             # Add rows for each issue
             for issue in issues:
                 line_str = str(issue["line"])
@@ -94,13 +91,5 @@ def print_flake8_report(report, markdown_file_path):
                 table.add_row(line_str, col_str, code, message)
                 # Add Markdown row
                 md_file.write(f"| {line_str} | {col_str} | {md_link(code, LINK_CODES)} | {message} |\n")
-
             # Print the table to console
             print(table)
-
-
-
-load_dotenv()
-report = flake8_to_dict(os.getenv("DIR"))
-markdown_file_path = 'flake8_report.md'
-print_flake8_report(report, markdown_file_path)
